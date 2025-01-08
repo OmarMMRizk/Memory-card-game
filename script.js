444fdec15a194742d229d198a5d863f1239936e4
Abdo-hasen <abdohasen200@gmail.com>
2025-01-08 20:55:36 +0200

    fix/reset button

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const gameCards = [
-    { id: 1, src: 'assets/Game cards/dragon.png', name:"Dragon" },
-    { id: 2, src: 'assets/Game cards/Flower.png', name:"Flower" },
-    { id: 3, src: 'assets/Game cards/lugi.png', name:"Lugi" },
-    { id: 4, src: 'assets/Game cards/mario.png', name:"Mario" },
-    { id: 5, src: 'assets/Game cards/mushroom.png', name:"Mushroom" },
-    { id: 6, src: 'assets/Game cards/princess.png', name:"Princess" },
-    { id: 7, src: 'assets/Game cards/superloser.png', name:"Superloser" },
-    { id: 8, src: 'assets/Game cards/tine.png', name:"Tine" },
-    { id: 9, src: 'assets/Game cards/coin.png', name:"Coin" },
-    { id: 10, src: 'assets/Game cards/egg.png', name:"Egg" },
+  { id: 1, src: 'assets/Game cards/dragon.png', name:"Dragon" },
+  { id: 2, src: 'assets/Game cards/Flower.png', name:"Flower" },
+  { id: 3, src: 'assets/Game cards/lugi.png', name:"Lugi" },
+  { id: 4, src: 'assets/Game cards/mario.png', name:"Mario" },
+  { id: 5, src: 'assets/Game cards/mushroom.png', name:"Mushroom" },
+  { id: 6, src: 'assets/Game cards/princess.png', name:"Princess" },
+  { id: 7, src: 'assets/Game cards/superloser.png', name:"Superloser" },
+  { id: 8, src: 'assets/Game cards/tine.png', name:"Tine" },
+  { id: 9, src: 'assets/Game cards/coin.png', name:"Coin" },
+  { id: 10, src: 'assets/Game cards/egg.png', name:"Egg" },
 ];
 
 let flippedCards = [];
@@ -16,128 +16,124 @@ let matchedCards = [];
 let moves = 0;
 let timer;
 let seconds = 0;
-let currentLevel = null; // Initialize currentLevel to null
+let currentLevel = null;
 
-// Function to update the timer display
 function updateTimerDisplay() {
-    let minutes = Math.floor(seconds / 60);
-    let remainingSeconds = seconds % 60;
-    let formattedTime = String(minutes).padStart(2, '0') + ":" + String(remainingSeconds).padStart(2, '0');
-    document.getElementById('time').textContent = formattedTime;
-}
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  const formattedTime = `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+  document.getElementById('time').textContent = formattedTime;
+};
 
-// Function to start the timer
 function startTimer() {
-    timer = setInterval(function() {
-        seconds++;
-        updateTimerDisplay();
-    }, 1000);
-}
+  timer = setInterval(() => {
+      seconds++;
+      updateTimerDisplay();
+  }, 1000);
+};
 
-// Function to stop the timer
 function stopTimer() {
-    clearInterval(timer);
-}
+  clearInterval(timer);
+};
 
-// Function to reset the game
 function resetGame() {
-    stopTimer();
-    startGame(currentLevel);
+  stopTimer();
+  startGame(currentLevel);
 }
 
 const startGame = (level) => {
-    currentLevel = level;
+  currentLevel = level;
     // Show the reset button
-    document.getElementById('reset').style.display = 'inline-block';
-    let cardNum;
-    if(level === "easy"){
-        cardNum = 12;
-    } else if(level === "medium"){
-        cardNum = 16;
-    } else if(level === "hard"){
-        cardNum = 20;
-    }
-    const gameLevel = [...gameCards.slice(0, cardNum / 2), ...gameCards.slice(0, cardNum / 2)];
-    gameLevel.sort(() => 0.5 - Math.random());
+  document.getElementById('reset').style.display = 'inline-block';
+  let cardNum;
+  if(level === "easy"){
+      cardNum = 12;
+  } else if(level === "medium"){
+      cardNum = 16;
+  } else if(level === "hard"){
+      cardNum = 20;
+  }
+  const gameLevel = [...gameCards.slice(0, cardNum / 2), ...gameCards.slice(0, cardNum / 2)];
+  gameLevel.sort(() => 0.5 - Math.random());
 
-    const gameGrid = document.querySelector('#game-play-content');
-    gameGrid.innerHTML = '';
-    gameLevel.forEach((card) => {
-        const cardElement = document.createElement('div');
-        cardElement.classList.add('card');
-        cardElement.dataset.name = card.name;
-        cardElement.dataset.id = card.id;
-        cardElement.innerHTML = `
-            <img class="front-face img" src="${card.src}" alt="${card.name}" />
-            <img class="back-face img" src="assets/Game cards/cardback.png" alt="Back" />
-        `;
-        gameGrid.appendChild(cardElement);
-    });
+  const gameGrid = document.querySelector('#game-play-content');
+  gameGrid.innerHTML = '';
+  gameLevel.forEach((card) => {
+      const cardElement = document.createElement('div');
+      cardElement.classList.add('card');
+      cardElement.dataset.name = card.name;
+      cardElement.dataset.id = card.id;
+      cardElement.innerHTML = `
+          <img class="front-face img" src="${card.src}" alt="${card.name}" />
+          <img class="back-face img" src="assets/Game cards/cardback.png" alt="Back" />
+      `;
+      gameGrid.appendChild(cardElement);
+  });
 
-    const allCards = document.querySelectorAll('.card');
-    allCards.forEach((card) => card.addEventListener('click', handleCardClick));
-
-    // Reset everything when starting a new game
-    flippedCards = [];
-    matchedCards = [];
-    moves = 0;
-    seconds = 0;
-    updateTimerDisplay();
-
-    //Start Timer
-    startTimer();
+  const allCards = document.querySelectorAll('.card');
+  allCards.forEach((card) => card.addEventListener('click', handleCardClick));
+  
+   // Reset everything when starting a new game
+  flippedCards = [];
+  matchedCards = [];
+  moves = 0;
+  seconds = 0;
+  updateTimerDisplay();
+  
+  //Start Timer
+  startTimer();
 };
 
 const handleCardClick = (event) => {
-    //play flip sound
-    const clickedCard = event.target.closest('.card');
-    if (!clickedCard || flippedCards.includes(clickedCard) || matchedCards.includes(clickedCard)) {
-        return;
-    }
+  //play flip sound
+  const clickedCard = event.target.closest('.card');
+  if (!clickedCard || flippedCards.includes(clickedCard) || matchedCards.includes(clickedCard)) {
+      return;
+  }
 
-    clickedCard.classList.add('flipped');
-    flippedCards.push(clickedCard);
-    if (flippedCards.length === 2) {
-        moves++;
-        document.getElementById('moves').textContent = `Moves: ${moves}`;
+  clickedCard.classList.add('flipped');
+  flippedCards.push(clickedCard);
+  if (flippedCards.length === 2) {
+      moves++;
+      document.getElementById('moves').textContent = `Moves: ${moves}`;
 
-        const [firstCard, secondCard] = flippedCards;
-        if (firstCard.dataset.id === secondCard.dataset.id) {
-            matchedCards.push(firstCard, secondCard);
-            flippedCards = [];
-            //play match sound
-            if (matchedCards.length === document.querySelector('#game-play-content').children.length) {
-                stopTimer();
-                alert(`Congratulations! You won in ${moves} moves and ${document.getElementById('time').textContent} seconds.`);
-                //play win sound
-            }
-        } else {
-            setTimeout(() => {
-                firstCard.classList.remove('flipped');
-                secondCard.classList.remove('flipped');
-                flippedCards = [];
-                //play wrong sound
-            }, 1000);
-        }
-    }
+      const [firstCard, secondCard] = flippedCards;
+      if (firstCard.dataset.id === secondCard.dataset.id) {
+          matchedCards.push(firstCard, secondCard);
+          flippedCards = [];
+          //play match sound
+          if (matchedCards.length === document.querySelector('#game-play-content').children.length) {
+              stopTimer();
+              alert(`Congratulations! You won in ${moves} moves and ${document.getElementById('time').textContent} seconds.`);
+              //play win sound
+          }
+      } else {
+          setTimeout(() => {
+              firstCard.classList.remove('flipped');
+              secondCard.classList.remove('flipped');
+              flippedCards = [];
+              //play wrong sound
+          }, 1000);
+      }
+  }
 }
 
 const levelBtns = document.querySelectorAll('.level');
 levelBtns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        startGame(e.target.value);
-    });
+  btn.addEventListener('click', (e) => {
+      startGame(e.target.value);
+  });
 });
 
 const resetButton = document.getElementById('reset');
 resetButton.addEventListener('click', resetGame);
 
 const audioOfTheGame = document.querySelectorAll('audio');
-const volumeController = document.getElementById('sound-volume');
+const volumeController = document.getElementById('sound-valume');
 
 volumeController.addEventListener('input', () => {
-    const volume = volumeController.value / 100;
-    audioOfTheGame.forEach(audio => {
-        audio.volume = volume;
-    });
+  const volume = volumeController.value / 100;
+  audioOfTheGame.forEach(audio => {
+      audio.volume = volume;
+  });
 });
